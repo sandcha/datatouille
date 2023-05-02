c1b533a0fd9bbcc560e10fe36e7ea0ef03da295a
sandcha <sandcha@users.noreply.github.com>
2023-05-02 17:39:01 +0200

    Documente le contexte et la limite du fichier des collectivités à compétences départementales
    Pour notre usage nécessitant le département

--- elus/explore_collectivites_statut_particulier.py
+++ elus/explore_collectivites_statut_particulier.py
@@ -1,28 +1,53 @@
 from pandas import read_csv
 
+# Exploration des collectivité à statut particulier pour identifier les listes d'élus.
+# Nous avons besoin du code du département pour chaque élu.
+
+
+# Définition "collectivité à statut particulier" : 
+# Une collectivité à statut particulier est, en France, une collectivité territoriale
+# qui, en application de l'article 72 de la Constitution, ne relève pas des trois catégories
+#  que sont les communes, les départements et les régions, 
+# sans pour autant être une collectivité d'outre-mer régie par l'article 74 de la Constitution.
+# Src : https://fr.wikipedia.org/wiki/Collectivit%C3%A9_%C3%A0_statut_particulier
+
 
 # Liste des collectivités à statut particulier selon l'INSEE :
 # https://www.insee.fr/fr/information/3528272
+# sachant qu' "une numérotation spécifique a été introduite" :
 # La Ville de Paris (75C)
 # Le Département de Mayotte (976D)
 # La Métropole de Lyon (69M)
 # Les collectivités territoriales uniques de Martinique (972R) et de Guyane (973R)
 # La Collectivité de Corse (20R)
-
+#
 # Exception : La Collectivité européenne d’Alsace (6AE) 
-# n'est pas une collectivité à statut particulier
-# au sens de l’article 72 de la Constitution.
-
-
-# Liste des membres des assemblées des collectivités à statut particulier selon le RNE :
-PATH_RNE_COLLECTIVITES_STATUT_PARTICULIER="./rne-ma-20221216.csv"
+# suit la numérotation spécifique mais n'est pas une collectivité
+#  à statut particulier au sens de l’article 72 de la Constitution.
 
 
 # Code officiel géographique (COG) 01.2023,
 # Collectivités territoriales ayant les compétences départementales selon l'INSEE 
-# (conseils départementaux et collectivités territoriales à statut particuler) :
+# https://www.insee.fr/fr/information/6800675 :
 PATH_COG_COLLECTIVITES_COMPETENCES_DEPARTEMENTALES="./v_ctcd_2023.csv"
+# Mais le fichier contient aussi les conseils départementaux
+# en plus des collectivités territoriales à statut particulier
+# et les départements ne sont pas cités (la région l'est).
+# Colonnes :
+# CTCD = Code de la collectivité
+# REG = Code de la région
+# TNCC = Type de nom en clair
+# NCC = Nom en clair (majuscules)
+# NCCENR = Nom en clair (typographie riche)
+# LIBELLE = Nom en clair (typographie riche) avec article
 
+
+# Liste des membres des assemblées des collectivités à statut particulier selon le RNE :
+PATH_RNE_COLLECTIVITES_STATUT_PARTICULIER="./rne-ma-20221216.csv"
+# Inclut la Nouvelle-Calédonie sachant que d'après https://fr.wikipedia.org/wiki/Collectivit%C3%A9_%C3%A0_statut_particulier : 
+# "La Nouvelle-Calédonie, en raison de son statut sui generis, 
+# est parfois considérée comme une « collectivité à statut particulier d'outre-mer »,
+# mais relève du titre XIII de la Constitution et non de l'article 72."
 
 membres_collectivites_particulieres=read_csv(
     PATH_RNE_COLLECTIVITES_STATUT_PARTICULIER,
